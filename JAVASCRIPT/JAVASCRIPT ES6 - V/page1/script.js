3f0d763d81c54d5eceb86d9b6a231b554fd85371
myreds12 <dondond45@gmail.com>
2024-05-24 20:00:21 +0700

    Javascript 5

---
+++ JAVASCRIPT/JAVASCRIPT ES6 - V/page1/script.js
@@ -0,0 +1,9 @@
+// Pindahkan pendefinisian class Animal ke animal.js dan hapus code di bawah
+
+
+// Pindahkan pendefinisian class Dog ke dog.js dan hapus code di bawah
+
+
+// Code di bawah jangan diganggu karena bukan bagian dari class Animal ataupun Dog
+const dog = new Dog("Leo", 4, "Chihuahua");
+dog.info();
